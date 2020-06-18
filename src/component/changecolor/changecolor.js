557c2d1d766faad639a8a1891148f5e30f3b9418
imanmehni <imanmehni1991@gmail.com>
2020-06-18 12:31:15 +0430

    Updates

--- src/component/changecolor/changecolor.js
+++
@@ -1,5 +0,0 @@
-export default (page,defaultBackground,background,defaultColor,color)=>{
-    page.style.transition="background-color 1s ease-in-out"
-    page.style.background=page.style.background===background?defaultBackground:background
-    Array.from(page.children).forEach(e=>e.style.color=e.style.color===color?defaultColor:color)
-  }
--- src/component/fullpage/FullPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-export default class Fullpage extends Component {
-  render() {
-    const { children } = this.props;
-    return <div className={`fullpage ${this.props.className || ''}`}>
-        {children}
-    </div>
-  }
-}
